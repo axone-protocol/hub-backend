2a5e3bc665abee4cd807bb6fc32a4b86e9001963
yevhen.burkovskyi <yevhen.burkovskyi@computools.com>
2024-05-24 10:18:58 +0300

    fix: token historical chart order fix, moved cache init to service

--- src/modules/token/services/token.jobs.ts
+++ src/modules/token/services/token.jobs.ts
@@ -19,36 +19,6 @@ export class TokenJobs {
   @Cron('*/5 * * * *')
   async fetchNewPrice() {
     await this.service.updateHistoryPrice();
-    await this.cache.init();
-  }
-
-  @Cron('* */2 * * *')
-  async refreshDayCache() {
-    await this.cache.initDayCache();
-  }
-
-  @Cron('* */6 * * *')
-  async refreshWeekCache() {
-    await this.cache.initWeekCache();
-  }
-
-  @Cron('* * */1 * *')
-  async refreshMonthCache() {
-    await this.cache.initMonthCache();
-  }
-
-  @Cron('* * */3 * *')
-  async refreshThreeMonthCache() {
-    await this.cache.initThreeMonthCache();
-  }
-
-  @Cron('* * * */1 *')
-  async refreshYearCache() {
-    await this.cache.initYearCache();
-  }
-
-  @Cron('* * * */1 *')
-  async refreshAllCache() {
-    await this.cache.initAllCache();
+    await this.service.initTokenHistoricalCache();
   }
 }
