d1c545315046074b9ee4dfad61fe893f525a20e6
yevhen.burkovskyi <yevhen.burkovskyi@computools.com>
2024-06-19 13:59:29 +0300

    fix: added type verification for apr

--- src/core/lib/osmosis/osmosis.service.ts
+++ src/core/lib/osmosis/osmosis.service.ts
@@ -1,17 +1,18 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from "@nestjs/common";
 
-import { config } from '@core/config/config';
-import { createUrlParams } from '@utils/create-url-params';
+import { config } from "@core/config/config";
+import { createUrlParams } from "@utils/create-url-params";
 
-import { GetHistoricalChartDto } from './dtos/get-historical-chart.dto';
-import { Endpoints } from './enums/endpoints.enum';
-import { RouteParam } from './enums/route-param.enum';
-import { FailedResponse } from './responses/failed.response';
-import { GSFResponse } from './responses/generic-success-failed.response';
-import { HistoricalChartRes } from './responses/historical-chart.response';
-import { HttpService } from '../http.service';
-import { TokenInfoResponse } from './responses/token-info.response';
-import { McapResponse } from './responses/mcap.response';
+import { GetHistoricalChartDto } from "./dtos/get-historical-chart.dto";
+import { Endpoints } from "./enums/endpoints.enum";
+import { RouteParam } from "./enums/route-param.enum";
+import { FailedResponse } from "./responses/failed.response";
+import { GSFResponse } from "./responses/generic-success-failed.response";
+import { HistoricalChartRes } from "./responses/historical-chart.response";
+import { HttpService } from "../http.service";
+import { TokenInfoResponse } from "./responses/token-info.response";
+import { McapResponse } from "./responses/mcap.response";
+import { Log } from "@core/loggers/log";
 
 @Injectable()
 export class OsmosisService {
@@ -20,38 +21,33 @@ export class OsmosisService {
   constructor(private readonly httpService: HttpService) {}
 
   private constructUrl(endpoint: string, params?: string): string {
-    return `${this.BASE_URL}/${endpoint}${params ? `?${params}` : ''}`;
+    return `${this.BASE_URL}/${endpoint}${params ? `?${params}` : ""}`;
   }
 
   private getWithErrorHandling<T>(url: string): Promise<T> {
-    return this.errorHandleWrapper(
-      this.httpService.get.bind(
-        null,
-        url,
-      ),
-    );
+    return this.errorHandleWrapper(this.httpService.get.bind(null, url));
   }
 
   async getHistoricalChart(
-    payload: GetHistoricalChartDto,
+    payload: GetHistoricalChartDto
   ): Promise<HistoricalChartRes> {
     const endpoint = Endpoints.HISTORICAL_PRICE.replace(
       RouteParam.SYMBOL,
-      payload.symbol,
+      payload.symbol
     );
 
     return this.getWithErrorHandling(
       this.constructUrl(
         endpoint,
-        createUrlParams({ tf: payload.range.toString() }),
-      ),
+        createUrlParams({ tf: payload.range.toString() })
+      )
     );
   }
 
   async getTokenInfo(symbol: string): Promise<TokenInfoResponse[]> {
     const endpoint = Endpoints.TOKEN_BY_SYMBOL.replace(
       RouteParam.SYMBOL,
-      symbol,
+      symbol
     );
 
     return this.getWithErrorHandling(this.constructUrl(endpoint));
@@ -61,10 +57,15 @@ export class OsmosisService {
     return this.getWithErrorHandling(this.constructUrl(Endpoints.MARKET_CAP));
   }
 
-  async getStakingApr(): Promise<number> {
-    return this.getWithErrorHandling(
-      this.constructUrl(Endpoints.STAKING_APR),
-    )
+  async getStakingApr(): Promise<string> {
+    const apr: number = await this.getWithErrorHandling(
+      this.constructUrl(Endpoints.STAKING_APR)
+    );
+    if (isNaN(apr)) {
+      Log.warn(`Got from Osmosis NaN for apr: ${apr}`);
+      return "0";
+    }
+    return apr.toString();
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -77,13 +78,15 @@ export class OsmosisService {
       }
 
       return response as T;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (e: any) {
       throw new BadRequestException(e.message);
     }
   }
 
-  private isFailedResponse<T>(response: GSFResponse<T>): response is FailedResponse {
+  private isFailedResponse<T>(
+    response: GSFResponse<T>
+  ): response is FailedResponse {
     return (response as FailedResponse).message !== undefined;
   }
 }
